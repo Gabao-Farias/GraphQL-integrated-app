926ee5c875cb7e73b889b2ef8e456b5938ed7a64
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-06-30 00:06:08 -0300

    adding resolvers

---
+++ backend/src/resolvers/user.ts
@@ -0,0 +1,22 @@
+import { randomUUID } from "node:crypto";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import UserModel from "../models/user";
+
+@Resolver()
+export default class UserResolver {
+  private data: UserModel[] = [];
+
+  @Query(() => [UserModel])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => UserModel)
+  async createUser(@Arg("name") name: string) {
+    const user = { id: randomUUID(), name };
+
+    this.data.push();
+
+    return user;
+  }
+}
